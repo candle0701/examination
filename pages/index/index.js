2bf84e08a14f9522f5f84b11e2453a469baf0fbe
candle0701 <18867946@qq.com>
2019-04-10 08:23:54 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,14 @@
+Page({
+  data: {
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    interval: 2000,
+    duration: 1000,
+    bannerImgs: ['http://img95.699pic.com/element/40093/3725.png_300.png', 'http://img95.699pic.com/element/40097/0928.png_300.png', 'http://img95.699pic.com/element/40095/0197.png_300.png'],
+    rankingList: [{ "id": 1, "title": "五年级练习", "introduction": "2019年夏季班语文同步练习期末冲刺", "price": "许昌学院校区", "url": "http://img95.699pic.com/element/40093/3731.png_300.png", "paynums": "158", "sort": "04-18 17:00" }, { "id": 2, "title": "五年级练习", "introduction": "2019年夏季班语文同步练习期末冲刺", "price": "许昌学院校区", "url": "http://img95.699pic.com/element/40093/3731.png_300.png", "paynums": "158", "sort": "04-18 17:00" }, { "id": 1, "title": "五年级练习", "introduction": "2019年夏季班语文同步练习期末冲刺", "price": "许昌学院校区", "url": "http://img95.699pic.com/element/40093/3731.png_300.png", "paynums": "158", "sort": "04-18 17:00" }, { "id": 1, "title": "五年级练习", "introduction": "2019年夏季班语文同步练习期末冲刺", "price": "许昌学院校区", "url": "http://img95.699pic.com/element/40093/3731.png_300.png", "paynums": "158", "sort": "04-18 17:00" }, { "id": 1, "title": "五年级练习", "introduction": "2019年夏季班语文同步练习期末冲刺", "price": "许昌学院校区", "url": "http://img95.699pic.com/element/40093/3731.png_300.png", "paynums": "158", "sort": "04-18 17:00" }]
+  },
+  onLoad: function() {
+
+  }
+})
