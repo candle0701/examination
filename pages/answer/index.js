aab39caa7f6ee6336bc35d77a46b4bd565581fc0
candle0701 <18867946@qq.com>
2019-04-16 17:36:06 +0800

    修改首页样式，数据修改为从后台获取

--- pages/answer/index.js
+++ pages/answer/index.js
@@ -5,16 +5,76 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    choiceList:[{
+      "choice_title":"昨天下雨了，一共多少滴？",
+      "choice_a":"30",
+      "choice_b":"60",
+      "choice_c":"90",
+      "choice_d":"1000",
+      "choice_image1":"http://img2.imgtn.bdimg.com/it/u=3154322533,1786994993&fm=26&gp=0.jpg",
+      "choice_sort": "1",
+      "choice_answer": "A"
+    },{
+        "choice_title": "大象如何装进冰箱？",
+        "choice_a": "打开冰箱门",
+        "choice_b": "把大象塞进去",
+        "choice_c": "关上冰箱门",
+        "choice_d": "以上三个步骤缺一不可",
+        "choice_image1": "",
+        "choice_sort": "2",
+        "choice_answer": "B"
+      }, {
+        "choice_title": "2019年NBA总冠军最终是哪支队伍？",
+        "choice_a": "只能是勇士",
+        "choice_b": "必须是勇士",
+        "choice_c": "早晚都是勇士",
+        "choice_d": "除了勇士就是勇士",
+        "choice_image1": "",
+        "choice_sort": "3",
+        "choice_answer": "C"
+      }, {
+        "choice_title": "冰与火之歌一共拍了多少季？",
+        "choice_a": "6",
+        "choice_b": "7",
+        "choice_c": "8",
+        "choice_d": "9",
+        "choice_image1": "",
+        "choice_sort": "4",
+        "choice_answer":"A"
+      }, {
+        "choice_title": "喝多少啤酒才能发胖？",
+        "choice_a": "一杯",
+        "choice_b": "一瓶",
+        "choice_c": "一箱",
+        "choice_d": "一车",
+        "choice_image1": "",
+        "choice_sort": "5",
+        "choice_answer": "D"
+      }]
   },
 
-  /**
+  /*
+  *
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
 
   },
-
+  formSubmit:function(e){
+    var arrStr = JSON.stringify(e.detail.value);
+    var arrStr = JSON.stringify(e.detail.value);
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    wx.showModal({
+      content: '确认要提交吗?',
+      success(res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,3 @@
-let rankList = [{ "id": 1, "title": "五年级练习", "introduction": "2019年夏季班语文同步练习期末冲刺", "price": "许昌学院校区", "url": "http://img95.699pic.com/element/40093/3731.png_300.png", "paynums": "158", "sort": "04-18 17:00", "actEndTime": '2019/05/01 10:00:43' }, { "id": 2, "title": "五年级练习", "introduction": "2019年夏季班语文同步练习期末冲刺", "price": "许昌学院校区", "url": "http://img95.699pic.com/element/40093/3731.png_300.png", "paynums": "158", "sort": "04-18 17:00", "actEndTime": '2019/05/01 10:00:43' }, { "id": 1, "title": "五年级练习", "introduction": "2019年夏季班语文同步练习期末冲刺", "price": "许昌学院校区", "url": "http://img95.699pic.com/element/40093/3731.png_300.png", "paynums": "158", "sort": "04-18 17:00", "actEndTime": '2019/05/01 10:00:43' }, { "id": 1, "title": "五年级练习", "introduction": "2019年夏季班语文同步练习期末冲刺", "price": "许昌学院校区", "url": "http://img95.699pic.com/element/40093/3731.png_300.png", "paynums": "158", "sort": "04-18 17:00", "actEndTime": '2019/05/01 10:00:43' }, { "id": 1, "title": "五年级练习", "introduction": "2019年夏季班语文同步练习期末冲刺", "price": "许昌学院校区", "url": "http://img95.699pic.com/element/40093/3731.png_300.png", "paynums": "158", "sort": "04-18 17:00", "actEndTime": '2019/05/01 10:00:43' }]
 Page({
   data: {
     countDownList: [],
@@ -9,56 +8,43 @@ Page({
     interval: 2000,
     duration: 1000,
     bannerImgs: ['http://img95.699pic.com/element/40093/3725.png_300.png', 'http://img95.699pic.com/element/40097/0928.png_300.png', 'http://img95.699pic.com/element/40095/0197.png_300.png'],
-    rankingList:[]
+    rankingList: [],
+    startdate:"2018-04-16",
+    enddate:"2018-04-16"
+  },
+  bindDateChange1(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      startdate: e.detail.value
+    })
+  },
+  bindDateChange2(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      enddate: e.detail.value
+    })
   },
   onLoad: function() {
-    let endTimeList = [];
-    // 将活动的结束时间参数提成一个单独的数组，方便操作
-    rankList.forEach(o => { endTimeList.push(o.actEndTime) })
-    this.setData({ actEndTimeList: endTimeList,rankingList: rankList });
-    // 执行倒计时函数
-    this.countDown();
+    var that = this;
+    that.selectByDate()
   },
-  timeFormat(param) {//小于10的格式化函数
-    return param < 10 ? '0' + param : param;
-  },
-  countDown() {//倒计时函数
-    // 获取当前时间，同时得到活动结束时间数组
-    let newTime = new Date().getTime();
-    let endTimeList = this.data.actEndTimeList;
-    let countDownArr = [];
-
-    // 对结束时间进行处理渲染到页面
-    endTimeList.forEach(o => {
-      let endTime = new Date(o).getTime();
-      let obj = null;
-      // 如果活动未结束，对时间进行处理
-      if (endTime - newTime > 0) {
-        let time = (endTime - newTime) / 1000;
-        // 获取天、时、分、秒
-        let day = parseInt(time / (60 * 60 * 24));
-        let hou = parseInt(time % (60 * 60 * 24) / 3600);
-        let min = parseInt(time % (60 * 60 * 24) % 3600 / 60);
-        let sec = parseInt(time % (60 * 60 * 24) % 3600 % 60);
-        obj = {
-          day: this.timeFormat(day),
-          hou: this.timeFormat(hou),
-          min: this.timeFormat(min),
-          sec: this.timeFormat(sec)
-        }
-      } else {//活动已结束，全部设置为'00'
-        obj = {
-          day: '00',
-          hou: '00',
-          min: '00',
-          sec: '00'
-        }
+  selectByDate:function(){
+    let that = this
+    wx.request({
+      url: 'http://localhost:8088/bank/selectByDate',
+      data: {
+        begin: '',
+        end: ''
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res) {
+        that.setData({
+          rankingList:res.data
+        })
       }
-      countDownArr.push(obj);
     })
-    // 渲染，然后每隔一秒执行一次倒计时函数
-    this.setData({ countDownList: countDownArr })
-    setTimeout(this.countDown, 1000);
   },
   toAnswer:function(){
     wx.navigateTo({
